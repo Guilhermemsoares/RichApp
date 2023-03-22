44aa610ec4e14bede960fcf511dfba9054657ac5
Guilhermemsoares <guigamsoares@hotmail.com>
2023-03-22 11:50:28 -0300

    pasta final

---
+++ i am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am Rich
+//
+//  Created by Guilherme Moreira Soares on 29/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
